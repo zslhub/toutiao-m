c45ba1381ef7c9b1eb823fac7b52c28869d9d23f
zslhub <2191055003@qq.com>
2022-10-01 19:17:45 +0800

    项目初始化-引入vant组件库

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './styles/less/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
